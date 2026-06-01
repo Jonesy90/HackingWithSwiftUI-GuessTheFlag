bbb70fde4555ad4093930d76d9d4e49ce3e58939
Michael Jones <michael.jones90@me.com>
2026-06-01 17:28:41 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-GuessTheFlag/HackingWithSwiftUI_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_GuessTheFlagApp.swift
+//  HackingWithSwiftUI-GuessTheFlag
+//
+//  Created by Michael Jones on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
